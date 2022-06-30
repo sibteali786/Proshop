da5ea41e3f28238a44904038c8244f1278a825bc
sibteali786 <sibteali786@gmail.com>
2022-07-01 00:58:00 +0500

    basic heder and footer setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className="App">
-      Welcome To Proshop
+      <Header />
+      <main className="py-3">
+        <Container>
+        <h3>Welcome To Proshop</h3>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
