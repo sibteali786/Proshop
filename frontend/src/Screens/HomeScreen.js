573325b6dd00d94e2b1245556668c337ac92a26b
sibteali786 <sibteali786@gmail.com>
2022-07-04 03:15:48 +0500

    resolved proxy error and fetched products from backend api

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,8 +1,20 @@
-import React from 'react'
-import products from './../products';
+import React, {useState, useEffect} from 'react'
 import {Row, Col} from "react-bootstrap";
 import Product from '../components/Product';
+import axios from "axios"
 const HomeScreen = () => {
+  const [products,setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () =>{
+      const {data} = await axios.get("/api/products")
+      setProducts(data);
+    }
+
+    fetchProducts();
+  },[])
+  // we go in swtupProxy.js and add explicit proxy when our local host converts to port 5000 from 3000 as backend is 
+  // running on 5000 port no. we also install npm install http-proxy-middleware --save. to be able to define a 
+  // proxy middle ware
   return (
     <>
     <h1>Latest Products</h1>
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,8 +1,87 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {Link} from "react-router-dom";
+import {Row, Col, Image, ListGroup, Card, Button} from "react-bootstrap";
+import Rating from './../components/Rating';
+import {useParams} from "react-router-dom";
+import axios from "axios"
+
+
 
 const ProductScreen = () => {
+  const match = useParams();
+  // fetching product from backend 
+  const [product,setProduct] = useState([]);
+  useEffect(() => {
+    const fetchProduct = async () =>{
+      const {data} = await axios.get(`/api/products/${match.id}`)
+      setProduct(data);
+    }
+
+    fetchProduct();
+  },[match])
+  // Accessing the required product by matching id in the url
+  // const product = products.find(p=> p._id === match.id);
   return (
-    <div>ProductScreen</div>
+    <>
+      <Link className='btn btn-dark my-3' to="/"> 
+        Go Back 
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} style={{width:"100%"}}/>
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={product.rating} text = {`${product.rating} reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price:${product.price}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Description:{product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  Price:
+                </Col>
+                <Col>
+                  <strong>
+                    ${product.price}
+                  </strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col>
+                  Status:
+                </Col>
+                <Col>
+                  <strong>
+                    ${product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                  </strong>
+                </Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button className="btn-block" type="button" disabled={product.countInStock === 0}>Add to Cart</Button>
+            </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>      
+        </Row>
+
+    </>
   )
 }
 
