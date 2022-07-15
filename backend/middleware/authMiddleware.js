ef7a8ced41f2dd1980022444c66eac6a6d0928b9
sibteali786 <sibteali786@gmail.com>
2022-07-16 01:47:45 +0500

    removed empty logs

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,9 +8,7 @@ const protect = expressAsyncHandler(async (req,res,next) => {
     if(req.headers.authorization  && req.headers.authorization.startsWith("Bearer")){
         try {
             token = req.headers.authorization.split(' ')[1];
-            console.log(token)
             const decoded = jwt.verify(token,process.env.JWT_SECRET);
-            console.log(decoded);
             req.user = await User.findById(decoded.id).select('-password')
             next()
         } catch (error) { 
