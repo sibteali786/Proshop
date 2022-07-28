e7aa85665910cd3686bed1268c2ee224cfe66b7a
sibteali786 <sibteali786@gmail.com>
2022-07-28 11:09:01 +0500

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,10 @@ import express from "express"
 import dotenv from "dotenv"
 // MongoDB configuration 
 import connectDB from "./config/db.js"
-import colors from 'colors';
 import { notFound,errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import path from 'path';
 dotenv.config(); // allows to use some variables defined accross whole app
 
 
@@ -18,7 +18,18 @@ app.get("/", (req, res) => {
   res.send("Api is Running......");
 });
 
-//using productRoutes 
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname,'frontend/build')))
+
+  app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}else{
+  app.get("/", (req, res) => {
+    res.send("Api is Running......");
+  });
+}
+
+//using productRoutes and userRoutes
 app.use("/api/products",productRoutes)
 app.use('/api/users',userRoutes)
 
